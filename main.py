d0b4e25b6d842ed9a670cbea201a16f45e42679a
lTworek1 <ltworek@icloud.com>
2024-09-02 12:56:42 +0200

    words count add

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ def main():
 	try:
 		filePath = sys.argv[1]
 	except:
-		filePath = input("File :Path: ")	
-
+		filePath = input("File Path: ")	
+	
 	with open(filePath) as f:
 		file_contents = f.read()
-		print(file_contents)
+		# print(file_contents)
+
+		words = file_contents.split()
+		countW = 0
+		for word in words:
+
+			countW += 1
+
+		print(f'In this file is {countW} words')
+
+
 main()
