aef986d073d4cff435aed8ff4b1658e241a9bb5d
lTworek1 <ltworek@icloud.com>
2024-09-02 13:23:40 +0200

    character count added

--- main.py
+++ main.py
@@ -11,11 +11,29 @@ def main():
 
 		words = file_contents.split()
 		countW = 0
+		charCount = {}
 		for word in words:
 
 			countW += 1
-
+			
+			lowWord = word.lower()
+			for char in lowWord:
+				try:
+					charC = charCount[char]
+				except:
+					charC = 0
+				
+				charC += 1	
+				charCount.update({char:charC}
+) 
 		print(f'In this file is {countW} words')
+		print('Characters Count:')
+		printStr = ""
+		for char in sorted(charCount):
+			countC = charCount[char]
+			printStr = f'{printStr}["{char}"]: {countC}; '
+		
+		print(printStr)
 
 
 main()
