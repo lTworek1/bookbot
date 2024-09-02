f7e4e37e94d932d9c8ed68dedb2a39eac264fab0
lTworek1 <ltworek@icloud.com>
2024-09-02 12:47:23 +0200

    file path as argument or user input

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
 def main():
-	with open("books/frankenstein.txt") as f:
+	try:
+		filePath = sys.argv[1]
+	except:
+		filePath = input("File :Path: ")	
+
+	with open(filePath) as f:
 		file_contents = f.read()
 		print(file_contents)
 main()
